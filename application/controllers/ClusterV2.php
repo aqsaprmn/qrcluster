ae44019c730c3fac0f2d9d463561f72ab17c945b
aqsaprmn <aqshapermana@gmail.com>
2022-07-29 16:05:41 +0700

    Salah Class

--- application/controllers/ClusterV2.php
+++ application/controllers/ClusterV2.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Cluster_S2 extends CI_Controller
+class ClusterV2 extends CI_Controller
 {
     public function __construct()
     {
